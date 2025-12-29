ef32ba991a5362cac148bf4fd37594b6bd3d14c8
HoangPhuc <phucnguyeho@gmail.com>
2025-12-29 18:00:56 +0700

    minimal learnable app

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet, Button, Text } from 'react-native';
+import { View, StyleSheet, Button, Text, Pressable } from 'react-native';
 import { useAudioPlayer } from 'expo-audio';
 import { apiCall } from "@/api/client";
 import { useEffect, useState } from 'react';
@@ -8,20 +8,23 @@ import { brickAudioUrl } from '@/api/endpoints';
 export default function Index() {
   const [brick, setBrick] = useState<Brick | null>(null);
   const [audioUri, setAudioUri] = useState<string | null>(null);
-  const player = useAudioPlayer(audioUri? {uri: audioUri}: null);
+  const player = useAudioPlayer(audioUri ? {uri: audioUri} : null);
+  const [showTarget, setShowTarget] = useState<boolean>(true);
+  const [showNative, setShowNative] = useState<boolean>(true);
+
+  const fetchRandomBrick = async () => {
+    try {
+      const br = await apiCall("/bricks/brick/random");
+      setBrick(br);
+      setAudioUri(brickAudioUrl(br.target_audio_url));
+    }
+    catch (err) {
+      console.error("Failed to fetch brick:", err);
+    }
+  };
 
   useEffect(() => {
-    const fetchBrick = async () => {
-      try {
-        const brick = await apiCall("/bricks/brick/2");
-        setBrick(brick);
-        setAudioUri(brickAudioUrl(brick.target_audio_url));
-      }
-      catch (err) {
-        console.error("Failed to fetch brick:", err);
-      }
-    }
-    fetchBrick();
+    fetchRandomBrick();
   }, []);
 
   return (
@@ -29,9 +32,15 @@ export default function Index() {
       {
         brick?
         <>
-          <Text style={{ fontSize: 24 }}>{brick.target_text}</Text>
-          <Text style={{ color: "#666" }}>{brick.native_text}</Text>
-          <Text>{brick.target_audio_url}</Text>
+          <Pressable onPress={() => setShowTarget(!showTarget)}>
+            <Text style={{ fontSize: 24, color: "green"}}>{showTarget ? brick.target_text : "***"}</Text>
+          </Pressable>
+          
+          <Pressable onPress={() => setShowNative(!showNative)}>
+            <Text style={{ fontSize: 24, color: "blue"}} >{showNative ? brick.native_text : "***"}</Text>
+          </Pressable>
+          
+          <Text style={{ fontSize: 9, color: "#666" }}>{brick.target_audio_url}</Text>
         </>:
         <Text>Loadding...</Text>
       }
@@ -44,6 +53,12 @@ export default function Index() {
           player?.play();
         }}
       />
+
+      <Button
+        title='Next'
+        onPress={fetchRandomBrick}
+      />
+
     </View>
   );
 }
--- config/env.ts
+++ config/env.ts
@@ -1,1 +1,1 @@
-export const API_BASE_URL = "http://10.0.2.2:8000";
+export const API_BASE_URL = "http://192.168.121.230:8000";
